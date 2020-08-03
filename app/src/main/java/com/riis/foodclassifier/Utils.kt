60fccffca39ea3e6e57e098ae41ac4774a4554ec
unknown <reedyjw1@udmercy.edu>
2020-08-03 09:18:03 -0400

    added navigation menu

---
+++ app/src/main/java/com/riis/foodclassifier/Utils.kt
@@ -0,0 +1,33 @@
+package com.riis.foodclassifier
+
+import android.graphics.Bitmap
+
+class Utils {
+
+    companion object {
+        fun resize(srcBmp: Bitmap, inputSize: Int): Bitmap{
+            if (srcBmp.width >= srcBmp.height){
+
+                val finalBitmap = Bitmap.createBitmap(
+                    srcBmp,
+                    srcBmp.width/2 - srcBmp.height/2,
+                    0,
+                    srcBmp.height,
+                    srcBmp.height
+                )
+                return Bitmap.createScaledBitmap(finalBitmap, inputSize, inputSize, false)
+
+            }else{
+
+                val finalBitmap = Bitmap.createBitmap(
+                    srcBmp,
+                    0,
+                    srcBmp.height/2 - srcBmp.width/2,
+                    srcBmp.width,
+                    srcBmp.width
+                )
+                return Bitmap.createScaledBitmap(finalBitmap, inputSize, inputSize, false)
+            }
+        }
+    }
+}
